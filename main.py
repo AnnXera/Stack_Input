e06d31b3df8ccb8afc70a7ecbf1f2cf98c22f295
Gian <solivarogiann@gmail.com>
2024-09-25 17:47:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+def ask_user_stacks():
+    try:
+        return int(input('Enter how many stacks you want to enter: '))
+    except ValueError:
+        return ask_user_stacks()
+
+
+stacks = []
+result = ''
+stacks_number = ask_user_stacks()
+
+for number in range(stacks_number):
+    stack_name = input(f'Enter stack #{number+1}: ')
+    stacks.append(stack_name)
+
+for index in range(len(stacks)):
+    if len(stacks) == 1:
+        result = 'stack of ' + stacks[0]
+        print(f'You have {result}.')
+    elif index == len(stacks)-1:
+        result = result + 'and ' + stacks[index]
+        print(f'You have stacks {result}.')
+    else:
+        result = result + stacks[index] + ', '
+
+while True:
+    ask_remove = input('Click R to remove an item: ')
+    if ask_remove == 'R':
+        if stacks:
+
+            stacks.pop()
+            result = ''
+
+            for index in range(len(stacks)):
+                if len(stacks) == 1:
+                    result = stacks[0]
+                elif index == len(stacks) - 1:
+                    result = result + 'and ' + stacks[index]
+                else:
+                    result = result + stacks[index] + ', '
+
+            if len(stacks) == 1:
+                print(f'You have stack {result}.')
+            elif stacks:
+                print(f'You have stacks {result}.')
+            else:
+                print('You do not have any item left on your stack.')
+                break
+    else:
+        continue
